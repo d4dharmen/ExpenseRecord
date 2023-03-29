0f0f9f93a4ee43c8a78605c8c5434558fa41469c
dharmendra <d4dharmen@gmail.com>
2023-03-29 11:40:54 -0700

    expenses add form added, plain form

--- src/components/forms/Forms.jsx
+++ src/components/forms/Forms.jsx
@@ -2,8 +2,16 @@ import "./forms.css";
 
 const Forms = () => {
   return (
-    <div className="forms">
-      <h2>forms</h2>
+    <div className="container">
+      <h3> Add your expenses here</h3>
+      <div className="forms">
+        <form action="">
+          <input type="text" placeholder="item for which expenced" />
+          <input type="number" name="" id="" placeholder="amount" />
+          <input type="date" name="" id="" defaultValue={new Date()} />
+          <button>Add expenses</button>
+        </form>
+      </div>
     </div>
   );
 };
